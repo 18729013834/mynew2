a2b1cd19d313123da7bf3704c1df516412d13629
18729013834 <69893744+18729013834@users.noreply.github.com>
2020-09-02 15:17:35 +0800

    Update test333.cs

--- yuhs.Entities/test333.cs
+++ yuhs.Entities/test333.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using MetaShare.Common.Core.Entities;
 /*add customized code between this region*/
+ Public string Name {get;set;}
 /*add customized code between this region*/
 
 namespace yuhs.Entities
@@ -10,6 +11,7 @@ namespace yuhs.Entities
 	public class test333 : MetaShare.Common.Core.Entities.Common
 	{
 		/*add customized code between this region*/
+                <add key="mqttUserName" value="admin"/>
 		/*add customized code between this region*/
 	}
 }
